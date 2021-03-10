b745dd16a00f74f6b44e4936977552b7f4261e16
Fabians <fabians.printful@gmail.com>
2021-03-10 19:21:47 +0200

    Extra changess

--- packages/vue-apollo/src/components/ApolloQuery.js
+++ packages/vue-apollo/src/components/ApolloQuery.js
@@ -153,14 +153,16 @@ export default {
         ...this.options,
         manual: true,
         result (result) {
-          const { loading, networkStatus, error } = result
+          const { loading, networkStatus, errors } = result
+          let { error } = result
           result = Object.assign({}, result)
 
-          // Unnecessary error rewriting:
-          // if (errors && errors.length) {
-          //   error = new Error(`Apollo errors occurred (${errors.length})`)
-          //   error.graphQLErrors = errors
-          // }
+          // Overwriting error only if it doesn't already exist
+          if (!error && errors && errors.length) {
+            const msg = errors.length > 1 ? `GraphQL errors occurred: (${errors.length})` : errors[0].message
+            error = new Error(msg)
+            error.graphQLErrors = errors
+          }
 
           let data = {}
 
